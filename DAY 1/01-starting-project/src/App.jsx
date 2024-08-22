caf8013ef61abc10c5451190e4679b7190eb10e8
QAISER ALI <qaisers.official@gmail.com>
2024-08-22 18:20:09 +0500

    store the big line in variable and use it in paragraph where i needed

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -3,13 +3,13 @@ function genrandomint(high){
   return Math.floor(Math.random() * (high + 1));
 } 
 function Header() {
+  const description = reactdetail[genrandomint(2)];
   return(
-   
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactdetail[genrandomint(2)]} React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
