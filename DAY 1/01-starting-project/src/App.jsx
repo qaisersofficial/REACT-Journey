6bb5fc10a661c26d46ae270f9f18b7eebdc8b0fb
QAISER ALI <qaisers.official@gmail.com>
2024-08-23 14:46:50 +0500

    import header into app.jsx and finally output it

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -1,20 +1,5 @@
-const reactdetail = ['Fundamental', 'Crucial', 'Core'];
-function genrandomint(high){
-  return Math.floor(Math.random() * (high + 1));
-} 
-function Header() {
-  const description = reactdetail[genrandomint(2)];
-  return(
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
+import React, {Component} from "react";
+import { Header } from "./Components/Header";
 function App() {
   return (
     <div>
