9d6e5cdd1070c50fcc12779b712eb32c65006915
QAISER ALI <qaisers.official@gmail.com>
2024-08-26 00:18:00 +0500

    understanding about props in components that how to access anything and how to reuse the same component with different data

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -1,6 +1,10 @@
 // import React, {Component} from "react";
 // import { Header } from "./Components/Header";
 import reactimg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import configImg from "./assets/config.png";
+import js_ui from "./assets/jsx-ui.png";
+import statemanagement from "./assets/state-mgmt.png";
 const reactdetail = ['Fundamental', 'Crucial', 'Core'];
 function genrandomint(high){
   return Math.floor(Math.random() * (high + 1));
@@ -19,12 +23,40 @@ function genrandomint(high){
     </header>
   );
 }
+
+const CoreConcept = (props) => {
+
+  return (
+    <li>
+      <img src = {props.image}  alt= {props.title} />
+      <h3>{props.Title}</h3>
+      <p>{props.Description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Lets get stated!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept Title="Fundamental"
+              Description = "The core concepts of React"
+              image = {componentsImg} />
+              <CoreConcept Title="Configurable"
+              Description = " React concepts you will need for almost any app you are going to build!"
+              image = {configImg}/>
+              <CoreConcept Title="JSX/UI"
+              Description = "React provides you with the JSX UI"
+              image = {js_ui}/>
+              <CoreConcept Title="State Management"
+              Description = "React concepts will amerge you about state management"
+              image = {statemanagement}/>
+          </ul>
+        </section>
+       
       </main>
     </div>
   );
