7c2d52216aeffdd62659531b0524412534fcd47e
QAISER ALI <qaisers.official@gmail.com>
2024-08-26 12:56:53 +0500

    Another way to shorter the component key to access all the element of the Array in this case

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -37,26 +37,33 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcept 
+          {/* <CoreConcept 
           title={CORE_CONCEPTS[0].title}
           description = {CORE_CONCEPTS[0].description}
           image = {CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept 
+          /> */}
+          <CoreConcept {...CORE_CONCEPTS[0]}/>
+          {/* <CoreConcept 
           title = {CORE_CONCEPTS[1].title}
           description = {CORE_CONCEPTS[1].description}
           image = {CORE_CONCEPTS[1].image}
-          />
-          <CoreConcept 
+          /> */}
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+
+          {/* <CoreConcept 
           title = {CORE_CONCEPTS[2].title}
           description = {CORE_CONCEPTS[2].description}
           image = {CORE_CONCEPTS[2].image}
-          />
-          <CoreConcept 
+          /> */}
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+
+          {/* <CoreConcept 
           title = {CORE_CONCEPTS[3].title}
           description = {CORE_CONCEPTS[3].description}
           image = {CORE_CONCEPTS[3].image}
-          />
+          /> */}
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
+
           </ul>
         </section>
        
