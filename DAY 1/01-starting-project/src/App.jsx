ff02a79e9a89f8f86e212a0ddffe366b003208ad
QAISER ALI <qaisers.official@gmail.com>
2024-08-26 18:34:10 +0500

    Best practice to storing components in files & using a good structure

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header.jsx';
+import { CORE_CONCEPTS } from "./Componentsdata";
+import CoreConcept from './components/CoreConcept.jsx';
 function App() {
   return (
     <div>
@@ -6,7 +9,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            
+
           {/* <CoreConcept 
           title={CORE_CONCEPTS[0].title}
           description = {CORE_CONCEPTS[0].description}
