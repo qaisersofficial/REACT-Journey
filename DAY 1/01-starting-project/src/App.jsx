2301add793bfff9a31f6f236405435f1bb3b65ff
QAISER ALI <qaisers.official@gmail.com>
2024-08-28 15:28:12 +0500

    Deriving and outputting data based on the state(useState)

--- DAY 1/01-starting-project/src/App.jsx
+++ DAY 1/01-starting-project/src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from "./Componentsdata";
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import ClickButton from './components/Button/ClickButton.jsx'
+import ClickButton from './components/Button/ClickButton.jsx';
+import { EXAMPLES } from './Componentsdata'; 
 function App() {
-  let tabContent = "Please click Any button";
+  // Hook and useState
+  const [selectedTopic, setSelectedtopic] =  useState('Components')
   function clickhandler(selectedbutton){
     // selectedbutton =>  Components Props States JSX
-  tabContent = selectedbutton;
-  alert ("You clicked " + tabContent + " Button");
+setSelectedtopic(selectedbutton);
 }
   return (
     <div>
@@ -38,12 +40,19 @@ function App() {
           <h2>Examples</h2>
               <menu>
                   <ClickButton  onTapped={() => clickhandler("Components")}>Components</ClickButton>
-                  <ClickButton onTapped={() => clickhandler("props")}>Props</ClickButton>
-                  <ClickButton onTapped={() => clickhandler("States")}>States</ClickButton>   {/* will learn it later*/}
+                  <ClickButton onTapped={() => clickhandler("Props")}>Props</ClickButton>
+                  <ClickButton onTapped={() => clickhandler("State")}>States</ClickButton>   {/* will learn it later*/}
                   <ClickButton onTapped={() => clickhandler("JSX")}>JSX</ClickButton>  
                          
               </menu>
-              {tabContent} 
+            
+             <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+             </div>
         </section >
       </main>
     </div>
