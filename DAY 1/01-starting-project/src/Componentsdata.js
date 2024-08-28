e46ae9d4f1ce383a81fda2aca7bacace74f7c670
QAISER ALI <qaisers.official@gmail.com>
2024-08-28 15:29:17 +0500

    updation in EXAMPLE object where i capitalized its keys first alphabet

--- DAY 1/01-starting-project/src/Componentsdata.js
+++ DAY 1/01-starting-project/src/Componentsdata.js
@@ -31,7 +31,7 @@ export const CORE_CONCEPTS = [
 ];
 
 export const EXAMPLES = {
-  components: {
+  Components: {
     title: 'Components',
     description:
       'Components are the building blocks of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.',
@@ -40,7 +40,7 @@ function Welcome() {
   return <h1>Hello, My Name is Qaiser and i am learning react !</h1>;
 }`,
   },
-  jsx: {
+  JSX: {
     title: 'JSX',
     description:
       'JSX is a syntax extension to JavaScript. It is similar to a template language, but it has full power of JavaScript (e.g., it may output dynamic content).',
@@ -50,7 +50,7 @@ function Welcome() {
   <p>Time to learn React!</p>
 </div>`,
   },
-  props: {
+  Props: {
     title: 'Props',
     description:
       'Components accept arbitrary inputs called props. They are like function arguments.',
@@ -59,7 +59,7 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }`,
   },
-  state: {
+  State: {
     title: 'State',
     description:
       'State allows React components to change their output over time in response to user actions, network responses, and anything else.',
