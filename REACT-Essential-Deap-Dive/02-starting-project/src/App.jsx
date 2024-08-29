3941f86f9e4134a7ac59639c5f474a6359ab62b4
QAISER ALI <qaisers.official@gmail.com>
2024-08-29 16:59:32 +0500

    Clean the App.jsx in efficient manners and learnt the workflow of react and how files and folders are mannaged in REACT

--- REACT-Essential-Deap-Dive/02-starting-project/src/App.jsx
+++ REACT-Essential-Deap-Dive/02-starting-project/src/App.jsx
@@ -1,43 +1,14 @@
-import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/Feature1-of-main/CoreConcepts.jsx';
-import ClickButton from './components/Button/ClickButton.jsx';
-import { EXAMPLES } from './Componentsdata'; 
+import Examples from './components/Feature2-of-main/Examples.jsx';
 function App() {
-  // Hook and useState
-  const [selectedTopic, setSelectedtopic] =  useState()
-  function clickhandler(selectedbutton){
-    // selectedbutton =>  Components Props States JSX
-setSelectedtopic(selectedbutton);
-}
-var content = <p>Please Click any button to show topic</p>;
-if(selectedTopic) {
-  content = <div id="tab-content">
-  <h3>{EXAMPLES[selectedTopic].title}</h3>
-  <p>{EXAMPLES[selectedTopic].description}</p>
-  <pre>
-    <code>{EXAMPLES[selectedTopic].code}</code>
-  </pre>
- </div>;
-}
+
   return (
     <>
       <Header />
       <main>
           <CoreConcepts/>
-
-        <section id="examples">
-          <h2>Examples</h2>
-              <menu>
-                  <ClickButton isClicked={selectedTopic=== 'Components'} onTapped={() => clickhandler("Components")}>Components</ClickButton>
-                  <ClickButton isClicked={selectedTopic=== 'Props'} onTapped={() => clickhandler("Props")}>Props</ClickButton>
-                  <ClickButton isClicked={selectedTopic=== 'State'}onTapped={() => clickhandler("State")}>States</ClickButton>   
-                  <ClickButton isClicked={selectedTopic=== 'JSX'} onTapped={() => clickhandler("JSX")}>JSX</ClickButton>  
-                         
-              </menu>
-            
-             {content}
-        </section >
+          <Examples/> 
       </main>
     </>
   );
