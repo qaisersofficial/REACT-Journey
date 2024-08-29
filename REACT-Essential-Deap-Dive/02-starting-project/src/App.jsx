eea7a4798a9b3d3d1053e23372f12530d8753547
QAISER ALI <qaisers.official@gmail.com>
2024-08-29 16:51:10 +0500

    Render the feature 1 in main and change in importing files

--- REACT-Essential-Deap-Dive/02-starting-project/src/App.jsx
+++ REACT-Essential-Deap-Dive/02-starting-project/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
-import { CORE_CONCEPTS } from "./Componentsdata";
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import CoreConcepts from './components/Feature1-of-main/CoreConcepts.jsx';
 import ClickButton from './components/Button/ClickButton.jsx';
 import { EXAMPLES } from './Componentsdata'; 
 function App() {
@@ -25,24 +24,7 @@ if(selectedTopic) {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-
-          {/* <CoreConcept 
-          title={CORE_CONCEPTS[0].title}
-          description = {CORE_CONCEPTS[0].description}
-          image = {CORE_CONCEPTS[0].image}
-          /> */}
-
-          {/* <CoreConcept {...CORE_CONCEPTS[0]}/> */}
-         {/* we can access the array element by this unique methode   */}
-          {CORE_CONCEPTS.map((conceptItem) => (
-            <CoreConcept key={conceptItem.title} {...conceptItem} />
-          ))}
-
-          </ul>
-        </section>
+          <CoreConcepts/>
 
         <section id="examples">
           <h2>Examples</h2>
