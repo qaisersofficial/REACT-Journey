e03252cec5da31fb699eb248e9f514a408bb85c7
QAISER ALI <qaisers.official@gmail.com>
2024-09-06 14:47:59 +0500

    Temporarily Player names are added and their symbols also

--- TIC TAK TOE GAME/src/App.jsx
+++ TIC TAK TOE GAME/src/App.jsx
@@ -1,7 +1,22 @@
 
 function App() {
   
-  return <p>Comming Soon...</p>
+  return (
+    <menu>
+      <div id="game-container"> 
+       <ol id="players">
+        <li>
+          <span className="player-name">Qaiser Ali</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Sharif Saleem</span>
+          <span className="player-symbol">O</span>
+        </li>
+       </ol>
+      </div>
+    </menu>
+  );
 }
 export default App
 
