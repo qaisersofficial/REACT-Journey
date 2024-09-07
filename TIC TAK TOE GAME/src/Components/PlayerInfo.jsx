80ce4cdf7c6e9d44d529531e22483fa0d891787f
QAISER ALI <qaisers.official@gmail.com>
2024-09-07 14:30:50 +0500

    creating player names using States and then dynamically access it and made modify button clickable

--- TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
+++ TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 function PlayerInfo ({name, symbol}) {
-
+   const [ isModification, setIsEditing ] = useState(false);
+      const  handleModificationClick = () =>{
+        setIsEditing(true);
+      }
+      let playerName = <span className="player-name">{name}</span>;
+      if (isModification){
+        playerName = <input type="text" required />;
+      }
     return (
     <li>
           <span className="player">
-            <span className="player-name">{name}</span>
+            {playerName}
             <span className="player-symbol">{symbol}</span>
           </span>
-          <button>Modify</button>
+          <button onClick={handleModificationClick}>Modify</button>
         </li>
         );
 }
