b438417ba78c95d59b8f5ec97be0f889e9681d72
QAISER ALI <qaisers.official@gmail.com>
2024-09-07 22:29:41 +0500

    Add GameBoard component in App.jsx to show the output

--- TIC TAK TOE GAME/src/App.jsx
+++ TIC TAK TOE GAME/src/App.jsx
@@ -1,4 +1,5 @@
 import PlayerInfo from './Components/Playerinfo';
+import GameBoard from './Components/GameBoard';
 function App() {
   
   return (<main>
@@ -7,6 +8,7 @@ function App() {
         <PlayerInfo initialName="Player 1" symbol="X"/>
         <PlayerInfo initialName="Player 2" symbol="O"/>
        </ol>
+       <GameBoard />
       </div>
     </main>
   );
