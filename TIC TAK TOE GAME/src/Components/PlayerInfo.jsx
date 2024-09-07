708a5f44ef8a1a4a79cd1cb9a1de8323350c1ad1
QAISER ALI <qaisers.official@gmail.com>
2024-09-07 15:08:48 +0500

    chore: Refactor PlayerInfo component to toggle between editing and displaying player name

--- TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
+++ TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 function PlayerInfo ({name, symbol}) {
    const [ isModification, setIsEditing ] = useState(false);
       const  handleModificationClick = () =>{
-        setIsEditing(true);
+        // setIsEditing(isModification ? false : true);
+        // setIsEditing(isModification ? false : true); // This is the same as the line above
+        setIsEditing(!isModification); // This is the same as the line above
       }
       let playerName = <span className="player-name">{name}</span>;
       if (isModification){
-        playerName = <input type="text" required />;
+        playerName = <input type="text" required value = {name} />;
       }
     return (
     <li>
@@ -14,7 +16,7 @@ function PlayerInfo ({name, symbol}) {
             {playerName}
             <span className="player-symbol">{symbol}</span>
           </span>
-          <button onClick={handleModificationClick}>Modify</button>
+          <button onClick={handleModificationClick}>{isModification ? "Save" : "Edit"} </button>
         </li>
         );
 }
