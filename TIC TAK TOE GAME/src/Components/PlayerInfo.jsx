e595f2708ec5c517f9fe5c455bf7298df2241cca
QAISER ALI <qaisers.official@gmail.com>
2024-09-07 15:33:56 +0500

    Refactor PlayerInfo component to toggle between editing and displaying player name by updating state based on old state Correctly

--- TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
+++ TIC TAK TOE GAME/src/Components/PlayerInfo.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 function PlayerInfo ({name, symbol}) {
    const [ isModification, setIsEditing ] = useState(false);
-      const  handleModificationClick = () =>{
-        // setIsEditing(isModification ? false : true);
-        // setIsEditing(isModification ? false : true); // This is the same as the line above
-        setIsEditing(!isModification); // This is the same as the line above
-      }
+    //   const  handleModificationClick = () =>{
+    //     // setIsEditing(isModification ? false : true);
+    //     // setIsEditing(isModification ? false : true); // This is the same as the line above
+    //     setIsEditing(!isModification); // This is the same as the line above
+    //   }
+    const handleModificationClick = () => {
+         setIsEditing((wasModification) => !wasModification);
+      };
       let playerName = <span className="player-name">{name}</span>;
       if (isModification){
         playerName = <input type="text" required value = {name} />;
