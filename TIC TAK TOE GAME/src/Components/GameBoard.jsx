636f588c866398b7a1e5d5a946e87474e8573ed8
QAISER ALI <qaisers.official@gmail.com>
2024-09-09 12:29:07 +0500

    Refactor GameBoard component to use useState hook for managing game board state and handle square clicks

--- TIC TAK TOE GAME/src/Components/GameBoard.jsx
+++ TIC TAK TOE GAME/src/Components/GameBoard.jsx
@@ -1,16 +1,26 @@
-const intiialGameBoard = [
+import { useState } from 'react';
+const intialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(intialGameBoard);
+    
+    function handleClickedSquare (rowIndex,columnIndex)  {
+        setGameBoard((prevGameBoard) => {
+            const newGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+            newGameBoard[rowIndex][columnIndex] = 'X';
+            return newGameBoard;
+        } );
+    }
     return (
         <ol id="game-board">
-        {intiialGameBoard.map((row, rowIndex) => (<li key={rowIndex} >
+        {gameBoard.map((row, rowIndex) => (<li key={rowIndex} >
             <ol>
                 {row.map((playerSymbol, columnIndex) => (<li key={columnIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleClickedSquare(rowIndex, columnIndex)}>{playerSymbol}</button>
                 </li>))}
             </ol>
         </li>))}
