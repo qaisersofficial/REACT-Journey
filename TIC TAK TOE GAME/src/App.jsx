a4d581cbfe0a986728073f65edc87f322bd6d37c
QAISER ALI <qaisers.official@gmail.com>
2024-09-10 11:45:16 +0500

    avoid intersecting states so that i commented out inn the gameboard component and created a log for checkout the move of the current player and in addition in App.jsx the log adn setlog function that use the useState Hook it will manage the clicks on the gameboard and give log to the users that inform the current user is playing i will manage two things with one state that will do manage the click and the log too

--- TIC TAK TOE GAME/src/App.jsx
+++ TIC TAK TOE GAME/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import PlayerInfo from './Components/Playerinfo';
+import PlayerInfo from './Components/PlayerInfo';
 import GameBoard from './Components/GameBoard';
+import Log from './Components/Log';
 function App() {
   const [activePlayer, setActivePlayer] = useState( "X" );
+  const [log, setLog] = useState([]);
   function switchPlayer() {
     setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
+    setLog();
   }
   
   return (<main>
@@ -15,6 +18,7 @@ function App() {
        </ol>
        <GameBoard onClickSquare={ switchPlayer } activePlayerSymbol={activePlayer}/>
       </div>
+      <Log />
     </main>
   );
 }
