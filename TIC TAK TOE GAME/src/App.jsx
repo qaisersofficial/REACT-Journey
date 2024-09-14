558858cae1aa50e4971cbf96fcc25b2c422448af
QAISER ALI <qaisers.official@gmail.com>
2024-09-14 23:44:54 +0500

    Complete and polish Tic-Tac-Toe game: Added player name change, background music, and enhanced game logic

--- TIC TAK TOE GAME/src/App.jsx
+++ TIC TAK TOE GAME/src/App.jsx
@@ -5,7 +5,12 @@ import ActivityLog from './Components/ActivityLog';
 import {WINNING_COMBINATIONS} from './winning-combitination';
 import GameOver from './Components/GameOver';
 import BackgroundMusic from './Components/BackgroundSong';
-const intialGameBoard = [
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -19,39 +24,41 @@ function derivedActivePlayer (gameTurns){
   return currentPlayer;
 }
 
+function deriveWinner(gameBoard,players){
+  let winner;
 
-function App() {
-  const [ players, setPlayers]=useState({
-        X: "Player 1",
-        O: "Player 2",
-  });
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState( "X" );
-  // const [hasWinner, setHasWinner] = useState(false);
-      const activePlayer = derivedActivePlayer(gameTurns);
-//cutted from GameBoard component...
-      let gameBoard = [...intialGameBoard.map(array => [...array])];
+  //lifting the computed value up
+      for(const combination of WINNING_COMBINATIONS){
+      const firstSquare = gameBoard[combination[0].row][combination[0].column];
+      const secondSquare = gameBoard[combination[1].row][combination[1].column];
+      const thirdSquare = gameBoard[combination[2].row][combination[2].column];
+        
+      if ( firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare){
+              winner = players[firstSquare];
+              break;
+          }
+      }
+      return winner;
+}
+
+    function deriveGameBoard(gameTurns){
+      let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
       for(const turn of gameTurns){
           const {square, player} = turn;
           const {row, col} = square;
           gameBoard[row][col] = player;
   
       }
-      let winner;
+      return gameBoard;
+    }
 
-      //lifting the computed value up
-          for(const combination of WINNING_COMBINATIONS){
-          const firstSquare = gameBoard[combination[0].row][combination[0].column];
-          const secondSquare = gameBoard[combination[1].row][combination[1].column];
-          const thirdSquare = gameBoard[combination[2].row][combination[2].column];
-            
-          if ( firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare){
-                  winner = players[firstSquare];
-                  break;
-              }
-          }
-
-          const hasDraw = gameTurns.length === 9 && !winner;
+function App() {
+  const [ players, setPlayers]=useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = derivedActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function switchPlayer(rowIndex, columnIndex) {
 
@@ -81,8 +88,8 @@ function App() {
      <BackgroundMusic src="/public/audio/bg-music.mp3" />
       <div id="game-container"> 
        <ol id="players" className='highlight-player'>
-        <PlayerInfo initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName = {handlePlayerNameChange}/>
-        <PlayerInfo initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName = {handlePlayerNameChange}/>
+        <PlayerInfo initialName={PLAYERS.X} symbol="X" isActive={activePlayer === 'X'} onChangeName = {handlePlayerNameChange}/>
+        <PlayerInfo initialName={PLAYERS.O} symbol="O" isActive={activePlayer === 'O'} onChangeName = {handlePlayerNameChange}/>
        </ol>
        {(winner || hasDraw )&& <GameOver winner={winner} onRestart={handleRematch}/>}
        <GameBoard onClickSquare={ switchPlayer } board={gameBoard}/>
@@ -92,4 +99,3 @@ function App() {
   );
 }
 export default App
-
