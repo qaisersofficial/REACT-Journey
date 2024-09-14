7813cb6c6ad07bae490f0175fd226873ed1d27f6
QAISER ALI <qaisers.official@gmail.com>
2024-09-14 14:58:13 +0500

    Refactor App component to derive active player from gameTurns state and remove redundant activePlayer state management

--- TIC TAK TOE GAME/src/App.jsx
+++ TIC TAK TOE GAME/src/App.jsx
@@ -2,19 +2,24 @@ import React, { useState } from 'react';
 import PlayerInfo from './Components/Playerinfo';
 import GameBoard from './Components/GameBoard';
 import ActivityLog from './Components/ActivityLog';
+
+function derivedActivePlayer (gameTurns){
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 function App() {
 
-  const [activePlayer, setActivePlayer] = useState( "X" );
+  // const [activePlayer, setActivePlayer] = useState( "X" );
   const [gameTurns, setGameTurns] = useState([]);
-
+      const activePlayer = derivedActivePlayer(gameTurns);
   function switchPlayer(rowIndex, columnIndex) {
-    setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
+    // setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
  
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+     const currentPlayer = derivedActivePlayer(prevTurns);
       const updatedTurns = [{ square: { row: rowIndex, col: columnIndex}, player: activePlayer },
          ...prevTurns
         ];
