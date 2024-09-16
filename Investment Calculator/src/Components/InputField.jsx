0ff317408e9f8b0bb1429d16d708d838bb1717b7
QAISER ALI <qaisers.official@gmail.com>
2024-09-16 12:24:12 +0500

    Refactor InputField component to include value and onChange props in UserInput.jsx

--- Investment Calculator/src/Components/InputField.jsx
+++ Investment Calculator/src/Components/InputField.jsx
@@ -1,8 +1,8 @@
-export default function InputField ({ label }){
+export default function InputField ({ label, value, onChange }){
     return (
         <p>
             <label >{label}</label>
-            <input type="number" required />
+            <input type="number" required  value={value} onChange={onChange}  />
         </p>
     );
 }
--- Investment Calculator/src/Components/UserInput.jsx
+++ Investment Calculator/src/Components/UserInput.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import InputField  from "./InputField";
 export default function UserInput (){
+   const [ userInput, setUserInput ] = useState({
+        initialInvestment: 50000,
+        annualInvestment: 2500,
+        expectedReturn: 10,
+        duration: 6
+    });
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((previousUserInput) => ({
+          ...previousUserInput,
+          [inputIdentifier]: newValue
+        }));
+      }
     return (
         <section id="user-input">
             <div className="input-group">
-        <InputField label="Initial Investment" />
-        <InputField label="Annual Investment" />
-        <InputField label="Expected Investment" />
-        <InputField label="Duration" />
+        <InputField label="Initial Investment" value = {userInput.initialInvestment}  onChange ={(event)=>handleChange("initialInvestment", event.target.value )} />
+        <InputField label="Annual Investment" value = {userInput.annualInvestment}  onChange ={(event)=>handleChange("annualInvestment", event.target.value )}/>
+        <InputField label="Expected Return" value = {userInput.expectedReturn}  onChange ={(event)=>handleChange("expectedReturn", event.target.value )} />
+        <InputField label="Duration" value = {userInput.duration} onChange ={(event)=>handleChange("duration", event.target.value )} />
             </div>
         </section>
     );
