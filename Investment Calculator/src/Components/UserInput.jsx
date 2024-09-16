62a28a5b356cc69ffd9571e35535a9aecec08122
QAISER ALI <qaisers.official@gmail.com>
2024-09-16 11:49:42 +0500

    the format for input fields created

---
+++ Investment Calculator/src/Components/UserInput.jsx
@@ -0,0 +1,24 @@
+export default function UserInput (){
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label >Initial Investment</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label >Annual Investment</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label >Expected Investment</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label > Duration </label>
+                    <input type="number" required />
+                </p>
+            </div>
+        </section>
+    );
+} 
