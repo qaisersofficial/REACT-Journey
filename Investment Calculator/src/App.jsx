ea69855435f72d4a002dcc1099307d2def6f04da
QAISER ALI <qaisers.official@gmail.com>
2024-09-16 13:39:35 +0500

    Refactor App component to include UserInput and InvestmentResults components
    - Lift up state from UserInput.jsx to App.jsx
    - Pass userInput and onChangeInput props to UserInput component
    - Add InvestmentResults component with input prop

--- Investment Calculator/src/App.jsx
+++ Investment Calculator/src/App.jsx
@@ -1,10 +1,28 @@
 import Header from "./Components/Header.jsx";
 import UserInput from "./Components/UserInput.jsx";
+import InvestmentResults from "./Components/InvestmentResults.jsx";
+import { useState } from "react";
 function App() {
+  const [ userInput, setUserInput ] = useState({
+    initialInvestment: 50000,
+    annualInvestment: 2500,
+    expectedReturn: 10,
+    duration: 6
+});
+function handleChange(inputIdentifier, newValue) {
+    setUserInput((previousUserInput) => ({
+      ...previousUserInput,
+      [inputIdentifier]: newValue
+    }));
+  }
   return (
    <>
     <Header />
-    <UserInput />
+    <UserInput 
+    userInput={userInput} 
+    onChangeInput = {handleChange}
+    />
+    <InvestmentResults input={userInput}/>
   </>)
 }
 
