cdffb2193112fb062ed89b74ad0724d2bf30ffb3
QAISER ALI <qaisers.official@gmail.com>
2024-10-12 15:47:41 +0500

    this is the how we can handle media queries and nested styling like h1 with styled components

--- Styling Components/Leaning styling using React/src/components/Header/Header.jsx
+++ Styling Components/Leaning styling using React/src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import logo from '../../assets/logo.png';
-import './Header.css';
+// import './Header.css';
 import classes from './Header.module.css';
 import styled from 'styled-components';
 const StyledHeaderComponent = styled.header `
-display: flex;
+    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     margin-top: 2rem;
     margin-bottom: 2rem;
+
+     @media (min-width: 768px) {
+  //  this is the way how we can use media query in styled components and we can also use it in the same way in css module with nested elements like h1, p etc
+      margin-bottom: 4rem;
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
 `
 const StyledImage = styled.img `
   object-fit: contain;
