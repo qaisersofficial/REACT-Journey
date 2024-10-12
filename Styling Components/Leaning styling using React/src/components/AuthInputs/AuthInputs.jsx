66d861475413934446ff639be10f7492c66a5ea5
QAISER ALI <qaisers.official@gmail.com>
2024-10-12 15:16:15 +0500

    Dynamice & conditional styling with styled Components

--- Styling Components/Leaning styling using React/src/components/AuthInputs/AuthInputs.jsx
+++ Styling Components/Leaning styling using React/src/components/AuthInputs/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label `
     font-weight: 700;
     letter-spacing: 0.1em;
     text-transform: uppercase;
-    color: #6b7280;
+    color: ${({$invalid})=>$invalid ? "#f87171" : "#6b7280"} ;
 `
 
 const Input = styled.input `
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color:  ${({$invalid})=>$invalid ? "#fed2d2" : "#d1d5db"} ;
+  color: ${({$invalid})=>$invalid ? "#ef4444" : "#374151" } ;
+  border: 1px solid ${({$invalid})=>$invalid ? "#f73f3f" : "transparent" };
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -52,7 +52,7 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p >
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
           // style={{
           //   backgroundColor: emailNotValid ? "#fed2d2" : "white",
@@ -60,12 +60,12 @@ export default function AuthInputs() {
           //   borderColor: emailNotValid ? '#f73f3f': 'black'
           // }}            
           type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+          $invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p >
-          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
           //  style={{
           //   backgroundColor: passwordNotValid ? "#fed2d2" : "white",
@@ -73,7 +73,7 @@ export default function AuthInputs() {
           //   borderColor: passwordNotValid ? '#f73f3f': 'black'
           // }}    
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
