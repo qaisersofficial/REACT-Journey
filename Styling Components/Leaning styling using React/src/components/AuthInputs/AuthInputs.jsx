9c75ac18da30f072d8ab7c9d4df6d92070c08120
QAISER ALI <qaisers.official@gmail.com>
2024-10-12 14:37:44 +0500

    Made the html element to the styled component by installing Styled-component use internal styling for the efficient pupose

--- Styling Components/Leaning styling using React/src/components/AuthInputs/AuthInputs.jsx
+++ Styling Components/Leaning styling using React/src/components/AuthInputs/AuthInputs.jsx
@@ -1,6 +1,33 @@
 import { useState } from 'react';
 import "./AuthInputs.css";
 // import classes from '../Header/Header.module.css';
+import styled from 'styled-components';
+const ControlContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
+const Label = styled.label `
+ display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: #6b7280;
+`
+
+const Input = styled.input `
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -23,10 +50,10 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p >
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</label>
-          <input
+          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</Label>
+          <Input
           // style={{
           //   backgroundColor: emailNotValid ? "#fed2d2" : "white",
           //   color: emailNotValid ? "#ef4444" : "black",
@@ -38,8 +65,8 @@ export default function AuthInputs() {
           />
         </p>
         <p >
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</label>
-          <input
+          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</Label>
+          <Input
           //  style={{
           //   backgroundColor: passwordNotValid ? "#fed2d2" : "white",
           //   color: passwordNotValid ? "#ef4444" : "black",
@@ -52,7 +79,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
--- Styling Components/Leaning styling using React/src/components/Header/Header.jsx
+++ Styling Components/Leaning styling using React/src/components/Header/Header.jsx
@@ -1,19 +1,45 @@
 import logo from '../../assets/logo.png';
 import './Header.css';
 import classes from './Header.module.css';
+import styled from 'styled-components';
+const StyledHeaderComponent = styled.header `
+display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+`
+const StyledImage = styled.img `
+  object-fit: contain;
+  margin-bottom: 2rem;
+  width: 11rem;
+  height: 11rem;
+`
+const StyledMainHeading = styled.h1 `
+  font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: 'Pacifico', cursive;
+    margin: 0;
+`
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <StyledHeaderComponent>
+      <StyledImage src={logo} alt="A canvas" />
+      <StyledMainHeading>ReactArt</StyledMainHeading>
       <p 
       // style={{
       //   color:"#a39191",
       //   textAlign:' center',
       //   margin: 0
       // }}
+      //this class name will same because i prefer here to use css module styling 
       className={classes.paragragh}
        >A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeaderComponent>
   );
 }
